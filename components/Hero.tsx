9b8aed7e366375d67a0d0806168f00a5545b4f7b
Jessica Kiroff <jesskiroff@gmail.com>
2025-01-02 19:16:55 -0500

    initialize hero file

---
+++ components/Hero.tsx
@@ -0,0 +1,10 @@
+// Hero typically represents the primary section above the fold, once one visits the page.
+import React from 'react';
+
+const Hero = () => {
+  return (
+  <div className='pb-20 pt-36'>Hero</div>
+)
+};
+
+export default Hero;
